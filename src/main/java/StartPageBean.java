101b64779c248572d7b741168959f6455cd01373
KayotPoziloy <yakhont04@mail.ru>
2023-12-10 16:40:44 +0300

    стартовая страница, бин обновления времени

---
+++ src/main/java/StartPageBean.java
@@ -0,0 +1,23 @@
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@ManagedBean
+@ViewScoped
+public class StartPageBean implements Serializable {
+
+    private String currentDateTime;
+
+    public String getCurrentDateTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        currentDateTime = dateFormat.format(new Date());
+        return currentDateTime;
+    }
+
+    public void updateTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        currentDateTime = dateFormat.format(new Date());
+    }
+}
