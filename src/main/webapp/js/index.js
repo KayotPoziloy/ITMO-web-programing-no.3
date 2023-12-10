a6c7438e83d3a87ed9ee80a0de3c238e13d370dc
KayotPoziloy <yakhont04@mail.ru>
2023-12-10 17:55:52 +0300

    на основную страницу добавлен канвас

---
+++ src/main/webapp/js/index.js
@@ -0,0 +1,132 @@
+const canvasPlot = document.getElementById('canvas_plot'); // инициализируем объект html
+const ctx = canvasPlot.getContext('2d'); // задаем полотно в 2d
+
+const canvasPlotWidth = canvasPlot.clientWidth;
+const canvasPlotHeight = canvasPlot.clientHeight;
+
+const lineWidth = 8;
+const lineHeight = 2;
+
+const xAxis = canvasPlotWidth / 2; // середина по горизонтали
+const yAxis = canvasPlotHeight / 2; // середина по вертикали
+
+const radius = 200;
+
+// функция обновляет канвас
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvasPlotWidth, canvasPlotHeight);
+    axis();
+    zone();
+    r();
+}
+clearCanvas();
+
+// Главные оси
+function axis() {
+    ctx.beginPath();
+    // ось Y
+    ctx.moveTo(xAxis, 0);
+    ctx.lineTo(xAxis, canvasPlotHeight);
+    // ось X
+    ctx.moveTo(0, yAxis);
+    ctx.lineTo(canvasPlotWidth, yAxis);
+
+    ctx.stroke();
+    ctx.closePath();
+}
+
+// Четверть круга
+function circle() {
+    ctx.beginPath();
+
+    ctx.arc(xAxis, yAxis, radius, 0, Math.PI/2);
+    ctx.lineTo(xAxis, yAxis);
+    ctx.fillStyle = "rgba(68,128,112,0.47)";
+    ctx.fill();
+
+    ctx.closePath();
+}
+
+// Треугольник
+function triangle() {
+
+    ctx.beginPath();
+    let x1 = xAxis;
+    let y1 = yAxis;
+    let x2 = xAxis;
+    let y2 = yAxis - radius / 2;
+    let x3 = xAxis - radius;
+    let y3 = yAxis;
+
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.lineTo(x3, y3);
+
+    ctx.fillStyle = "rgba(68,128,112,0.47)";
+    ctx.fill();
+    ctx.closePath();
+}
+
+// Квадрат
+function square() {
+    ctx.beginPath();
+    let x1 = xAxis - radius;
+    let y1 = yAxis;
+    let x2 = xAxis - radius;
+    let y2 = yAxis + radius / 2;
+    let x3 = xAxis;
+    let y3 = yAxis + radius / 2;
+    let x4 = xAxis;
+    let y4 = yAxis;
+
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.lineTo(x3, y3);
+    ctx.lineTo(x4, y4)
+
+    ctx.fillStyle = "rgba(68,128,112,0.47)";
+    ctx.fill();
+    ctx.closePath();
+}
+
+// Отрисовка ОДЗ
+function zone() {
+    circle();
+    triangle();
+    square();
+}
+
+// Отрисовка значений R
+function r() {
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    ctx.font = '10px Arial';
+    ctx.fillText("X", canvasPlotWidth-10, yAxis);
+    ctx.fillText("Y", xAxis, + 10);
+
+    ctx.font = '15px Arial';
+    for (let i = radius; i >= -(radius); i -= radius / 2 ) {
+
+        let litR = " -R"
+        let litR2 = " -R/2"
+
+        if (i < radius) {
+            litR = " R"
+            litR2 = " R/2"
+        }
+
+        if (i === radius / 2 || i === -radius / 2) {
+            ctx.fillText(litR2, xAxis - i, yAxis);
+            ctx.fillText(litR2, xAxis, yAxis + i);
+        } else if (i === radius || i === -(radius)) {
+            ctx.fillText(litR, xAxis - i, yAxis);
+            ctx.fillText(litR, xAxis, yAxis + i);
+        } else {
+            continue;
+        }
+        ctx.fillRect(xAxis - i, yAxis - 4, lineHeight, lineWidth);
+        ctx.fillRect(xAxis - 4, yAxis - i, lineWidth, lineHeight);
+
+    }
+    ctx.closePath();
+}
